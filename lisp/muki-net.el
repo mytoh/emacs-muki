cc84d300af3aee1c1b63f6927806d05e2a114c27
mytoh <mytoh@gmail.com>
2016-11-21 22:27:14 +0900

    * lisp/muki-net.el: add some commands

--- lisp/muki-net.el
+++ lisp/muki-net.el
@@ -13,7 +13,7 @@
               (node-filter (elms target attr)
                            (seq-filter
                             (lambda (elm) (string-equal target
-                                                   (xml-get-attribute elm attr)))
+                                                        (xml-get-attribute elm attr)))
                             elms)))
     (cl-letf* ((html (with-current-buffer
                          (url-retrieve-synchronously
@@ -61,7 +61,7 @@
               (node-filter (elms target attr)
                            (seq-filter
                             (lambda (elm) (string-equal target
-                                                   (xml-get-attribute elm attr)))
+                                                        (xml-get-attribute elm attr)))
                             elms)))
     (cl-letf* ((doc (with-current-buffer
                         (url-retrieve-synchronously
@@ -410,5 +410,141 @@
                        (candidates . ,(items->candidates items maxitemlength))
                        (action . ,actions))
             :buffer "*boot.fm*"))))
+
+(cl-defun muki:functional-geekery ()
+  (interactive)
+  (cl-labels ((format-title (title summary mx)
+                            (concat
+                             (propertize title 'face 'font-lock-string-face)
+                             (make-string (+ 3 (- mx (length title)))
+                                          ?\ )
+                             (if summary
+                                 (propertize summary 'face 'font-lock-keyword-face))
+                             ""))
+              (items->candidates (items mx)
+                                 (colle:map
+                                  (lambda (item)
+                                    (pcase-let ((`((title ,_ ,title))
+                                                 (thread-first item
+                                                   (xml-get-children 'title)))
+                                                (`((enclosure ((url . ,url) . ,_) . ,_) . ,_)
+                                                 (thread-first item
+                                                   (xml-get-children 'enclosure)))
+                                                (`((summary ,_ ,summary)
+                                                   (thread-first item
+                                                     (xml-get-children 'summary))))
+                                                )
+                                      (cons
+                                       (format-title title summary mx)
+                                       url)))
+                                  items)))
+    (pcase-let* ((feedurl "https://www.functionalgeekery.com/feed/mp3")
+                 (root (with-current-buffer
+                           (url-retrieve-synchronously
+                            feedurl)
+                         (libxml-parse-html-region
+                          (point-min) (point-max))))
+                 (`(html ,_
+                         (body ,_
+                               . ,body))
+                  root)
+                 (items (thread-first body
+                          (xml-get-children 'item)))
+                 (maxitemlength (apply #'max
+                                       (mapcar
+                                        (lambda (item)
+                                          (pcase-let ((`((title ,_ ,title))
+                                                       (xml-get-children item 'title)))
+                                            (length title)))
+                                        items)))
+                 (actions
+                  `(("Open" . (lambda (url)
+                                (muki:play-mpv url))))))
+
+      ;; (cl-letf ((buf (get-buffer-create "testthechagelog")))
+      ;;   (with-current-buffer buf
+      ;;     (delete-region (point-min) (point-max))
+      ;;     (prin1 (car items)(current-buffer))
+      ;;     (write-file "testchangelog"))
+      ;;   (switch-to-buffer buf))
+
+      (helm :sources `((name . "functional-geekery")
+                       (candidates . ,(items->candidates items maxitemlength))
+                       (action . ,actions))
+            :buffer "*functional geekery*")
+      )))
+(defun muki:nhk-world ()
+  "[[http://askubuntu.com/questions/704612/nhk-live-streaming-not-available-on-this-browser][firefox - NHK Live streaming not available on this browser - Ask Ubuntu]]"
+  (interactive)
+  (pcase-let* ((xmlurl "http://www3.nhk.or.jp/nhkworld/app/tv/hlslive_tv.xml")
+               (root (with-current-buffer
+                         (url-retrieve-synchronously
+                          xmlurl)
+                       (libxml-parse-html-region
+                        (point-min) (point-max))))
+               (`(html ,_
+                       (body ,_
+                             (p ,_ ,_ ,_
+                                ,urls)))
+                root)
+               (url (thread-first urls
+                      (xml-get-children 'main_url)
+                      car
+                      (xml-get-children 'jstrm)
+                      car
+                      cl-caddr)))
+    (muki:play-mpv url)))
+
+(defun muki:defn-audio ()
+  (interactive)
+  (cl-labels ((format-title (title mx)
+                            (concat
+                             (propertize title 'face 'font-lock-string-face)
+                             (make-string (+ 3 (- mx (length title)))
+                                          ?\ )))
+              (items->candidates (items mx)
+                                 (colle:map
+                                  (lambda (item)
+                                    (pcase-let ((`((title ,_ ,title))
+                                                 (thread-first item
+                                                   (xml-get-children 'title)))
+                                                (`((enclosure ((url . ,url) . ,_) . ,_) . ,_)
+                                                 (thread-first item
+                                                   (xml-get-children 'enclosure)))
+                                                )
+                                      (cons
+                                       (format-title title mx)
+                                       url)))
+                                  items)))
+    (pcase-let* ((feedurl "http://feeds.soundcloud.com/users/soundcloud:users:220484243/sounds.rss")
+                 
+                 (root (with-current-buffer
+                           (url-retrieve-synchronously
+                            feedurl)
+                         (libxml-parse-html-region
+                          (point-min) (point-max))))
+                 (`(html ,_
+                         (body ,_
+                               (p ,_ ,_ ,_
+                                  (rss ,_
+                                       ,channel))))
+                  root)
+                 (items (thread-first channel
+                          (xml-get-children 'item)))
+                 (maxitemlength (apply #'max
+                                       (mapcar
+                                        (lambda (item)
+                                          (pcase-let ((`((title ,_ ,title))
+                                                       (xml-get-children item 'title)))
+                                            (length title)))
+                                        items)))
+                 (actions
+                  `(("Open" . (lambda (url)
+                                (muki:play-mpv url))))))
+      (helm :sources `((name . "defn")
+                       (candidates . ,(items->candidates items maxitemlength))
+                       (action . ,actions))
+            :buffer "*defn*"))))
+
 (provide 'muki-net)
 ;;; muki-net.el ends here
